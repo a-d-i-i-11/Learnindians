11648a688292897c1a991c7709d8b8a51620da13
a-d-i-i-11 <104435380+a-d-i-i-11@users.noreply.github.com>
2026-05-22 11:21:04 +0530

    Update admin email in configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LI_CONFIG = {
   supabaseUrl: "https://qekcsdxzinraefaxjebu.supabase.co",
   supabaseAnonKey: "sb_publishable_Fbv7xV4p03QTA4Z0eY_1VQ_TNABeysk",
-  adminEmails: ["info@hirenix.co"],
+  adminEmails: ["khanadnanofficial432@gmail.com"],
 };
